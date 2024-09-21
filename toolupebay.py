8fb809525b0e234c50d71ea7448c9441ae265196
masterfrog99 <malucoprod51@gmail.com>
2024-09-21 23:32:57 +0100

    Removing more unused vars and updating some errors and a bit more comments

--- toolupebay.py
+++ toolupebay.py
@@ -1,13 +1,13 @@
-import re
 import requests
 from bs4 import BeautifulSoup
 
 url = 'https://www.ebay.com/sch/i.html?_nkw=playstation+5'
-useragent = {
+useragent = { # optional
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0'
-}
+} 
 response = requests.get(url, headers=useragent)
 
+# check if status code is 200 (successful)
 if response.status_code == 200:
     parse = BeautifulSoup(response.content, 'html.parser')
     
@@ -18,7 +18,7 @@ if response.status_code == 200:
         <head>
             <style>
                 @font-face {
-                    font-family: 'Ubuntu';
+                    font-family: 'Ubuntu';     # make sure you have fonts folder inside project folder
                     src: url('fonts/Ubuntu-Regular.ttf') format('truetype');
                     font-weight: normal;
                 }
@@ -36,29 +36,31 @@ if response.status_code == 200:
         </head>
         <body>
         """)
-        listings = parse.findAll('li', class_='s-item')
+        listings = parse.findAll('li', class_='s-item') # find all wanted listing objects in ebay
         
         # organizing wanted data and filtering output       
         for listing in listings:
             title_tag = listing.find('div', class_='s-item__title')
             price_tag = listing.find('span', class_='s-item__price')
             link_tag = listing.find('a', class_='s-item__link')
-    
+            
+            # format the data output to be more readable
             title = title_tag.get_text(strip=True)
             price = price_tag.get_text(strip=True)
             link = link_tag['href']
-            
-            price_bold = f"<strong>{price}</strong>"
-            output = output = f"""
+
+            # organize and format final result
+            price_bold = f"<strong>{price}</strong>" # optional var, can be added manually to output var
+            output = f"""
             <div class="listing">
-                {title} - <span class='price'>{price}</span> - <a href="{link}">eBay Link</a>
+                {title} - <span class='price'>{price_bold}</span> - <a href="{link}">eBay Link</a>
             </div>
             """
 
-            print(output.strip()) # optional
+            # print(output.strip())
             
-            # updating file
-            file.write(output)
-        file.write("<html><body>\n")
+            
+            file.write(output) # updating file
+        file.write("<html><body>\n") # final write
 else:
     print("Failed to retrieve the page")
